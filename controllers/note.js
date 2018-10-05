779e6e00357585a2775bdb4fe31ff7babf1924ac
Fuzoka <unbeatablesites09@gmail.com>
2018-10-05 02:44:27 -0400

    React and Heroku

--- controllers/note.js
+++ controllers/note.js
@@ -1,26 +1,19 @@
 const db = require('../models');
 
 exports.saveNote = function (req, res) {
-    // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
         .then(function (dbNote) {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
             return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id } }, { new: true });
         })
         .then(function (dbArticle) {
-            // If we were able to successfully update an Article, send it back to the client
             res.json(dbArticle);
         })
         .catch(function (err) {
-            // If an error occurred, send it to the client
             res.json(err);
         });
 }
 
 exports.deleteNote = function (req, res) {
-    //console.log("REQ.BODY",req.body);
     const note = req.body.note, article = req.body.article;
 
     db.Article.findById(article, "notes").then(function (result) {
